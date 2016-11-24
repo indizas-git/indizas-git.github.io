544d8268e20827b93540e9ebd1650ac28d48de47
Mark Granger <indizas-source@yahoo.com>
2016-11-24 18:31:04 +0000

    attempting first deployment

---
+++ config.js
@@ -0,0 +1,25 @@
+var APP_URL = 'https://pulstybsyqcvpxm.form.io';
+var API_URL = 'https://api.form.io';
+
+// Parse query string
+var query = {};
+location.search.substr(1).split("&").forEach(function(item) {
+    query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
+});
+
+APP_URL = query.appUrl || APP_URL;
+API_URL = query.apiUrl || API_URL;
+
+angular.module('formioSalesQuoteApp').constant('AppConfig', {
+  appUrl: APP_URL,
+  apiUrl: API_URL,
+  company: 'Acme Inc.',
+  forms: {
+    contractForm: APP_URL + '/contract',
+    customerForm: APP_URL + '/customer',
+    opportunityForm: APP_URL + '/opportunity',
+    agentForm: APP_URL + '/agent',
+    quoteForm: APP_URL + '/quote',
+    userLoginForm: APP_URL + '/user/login'
+  }
+});
---
+++ config.template.js
@@ -0,0 +1,19 @@
+{% if domain %}
+APP_URL = '{{ domain }}';
+API_URL = '{{ domain }}';
+{% else %}
+APP_URL = '{{ protocol }}://{{ path }}.{{ host }}';
+API_URL = '{{ protocol }}://api.{{ host }}';
+{% endif %}
+
+// Parse query string
+var query = {};
+location.search.substr(1).split("&").forEach(function(item) {
+    query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
+});
+
+angular.module('formioSalesQuoteApp').constant('AppConfig', {
+  appUrl: query.appUrl || APP_URL,
+  apiUrl: query.apiUrl || API_URL,
+  company: 'Acme Inc.'
+});
